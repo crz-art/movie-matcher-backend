8699f5b5362b2b6798262a35d897a3c95493fca4
crz-art <carlamart249@gmail.com>
2026-07-23 21:37:20 -0600

    Actualizar clave de superusuario

--- create_superuser.py
+++ create_superuser.py
@@ -9,9 +9,9 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 # Cambia estos datos por los que quieras usar para iniciar sesión
-USERNAME = 'admin'
-EMAIL = 'admin@example.com'
-PASSWORD = 'TuContraseñaSegura123'
+USERNAME = 'dino'
+EMAIL = 'carlamart249@gmail.com'
+PASSWORD = 'soyjochis12'
 
 if not User.objects.filter(username=USERNAME).exists():
     User.objects.create_superuser(username=USERNAME, email=EMAIL, password=PASSWORD)
