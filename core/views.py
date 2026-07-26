34676a9790cafc94cd01aebef31544dd604a817e
crz-art <carlamart249@gmail.com>
2026-07-25 18:14:39 -0600

    Agregar filtro de peliculas por genero en views

--- core/views.py
+++ core/views.py
@@ -1,19 +1,33 @@
 from rest_framework import viewsets
 from .models import Genero, Usuario, Pelicula, Watchlist
-from .serializers import GeneroSerializer, UsuarioSerializer, PeliculaSerializer, WatchlistSerializer
+from .serializers import (
+    GeneroSerializer,
+    UsuarioSerializer,
+    PeliculaSerializer,
+    WatchlistSerializer,
+)
 
 class GeneroViewSet(viewsets.ModelViewSet):
     queryset = Genero.objects.all()
     serializer_class = GeneroSerializer
 
+
 class UsuarioViewSet(viewsets.ModelViewSet):
     queryset = Usuario.objects.all()
     serializer_class = UsuarioSerializer
 
+
 class PeliculaViewSet(viewsets.ModelViewSet):
-    queryset = Pelicula.objects.all()
     serializer_class = PeliculaSerializer
+
+    def get_queryset(self):
+        queryset = Pelicula.objects.all()
+        genero_id = self.request.query_params.get('genero')
+        if genero_id:
+            queryset = queryset.filter(genero_id=genero_id)
+        return queryset
+
 
 class WatchlistViewSet(viewsets.ModelViewSet):
     queryset = Watchlist.objects.all()
-    serializer_class = WatchlistViewSet if False else WatchlistSerializer
+    serializer_class = WatchlistSerializer
